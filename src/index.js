fd40f16ec671248ea28e2e96d9f6d5ce2d16bc30
Lucas Gabriel <Lucas.gab1010@gmail.com>
2024-08-21 05:11:59 -0300

    Diretorios organizados + Criação de 404.html com sua devida router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require ("express")
 const app = express();
 const path = require ("path")
-const rotas = require('./itmasters')
+const rotas = require('./router')
 const port = 3000;
-const basePath = path.join (__dirname, "../html/")
+const basePath = path.join (__dirname, "../public/html/")
 
 
 app.use("/itmasters" , rotas)
@@ -11,8 +11,11 @@ app.get("/", (req,res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
 
-app.use(express.static(path.join(__dirname, "../assets/")));
+app.use(express.static(path.join(__dirname, "../public/")));
 
+app.use ( (req,res,next)=>{
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
 
 app.listen(port, ()=>{
     console.log(`Server aberto na porta ${port}`)
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 const express = require ("express")
 const router = express.Router();
 const path = require ("path")
-const basePath = path.join (__dirname, "../../html")
+const basePath = path.join (__dirname, "../../public/html")
 
-router.get("/home", (req,res)=>{
+router.get("/", (req,res)=>{
     res.sendFile(`${basePath}/inicio.html`)
 })
 
