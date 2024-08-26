014d6a924156ff29834cc3dae1b13a9c216b66be
Lucas Gabriel <100373893+Lukeehz@users.noreply.github.com>
2024-08-26 03:57:08 -0300

    Delete dependencias.sh

--- dependencias.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install -y nodejs
-curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install -g pm2
-sudo npm install express
-sudo npm install bcrypt dotenv mongoose jsonwebtoken
