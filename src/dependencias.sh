1efad50e6bf282e0aeb2ef10b9c343a9b8a12bf1
Lucas Gabriel <100373893+Lukeehz@users.noreply.github.com>
2024-08-26 03:56:53 -0300

    Create dependencias.sh

---
+++ src/dependencias.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm install -g pm2
+sudo npm install bcrypt dotenv mongoose jsonwebtoken express
